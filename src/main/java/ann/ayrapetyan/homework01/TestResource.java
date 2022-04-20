c217bc1afeb73fd65567279f52757e4e1792eb91
Айрапетян Анна <amarayrapetyan@sberbank.ru>
2022-04-20 14:03:39 +0300

    empty branch

--- src/main/java/ann/ayrapetyan/homework01/TestResource.java
+++
@@ -1,9 +0,0 @@
-package ann.ayrapetyan.homework01;
-
-import ann.ayrapetyan.homework01.data.Question;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TestResource {
-    Question getNextQuestion();
-}
